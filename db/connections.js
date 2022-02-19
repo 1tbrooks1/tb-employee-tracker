1ce7ae5e54b246ca9b36d5958ac3af32a04d84fe
tbrooks <tbrooks321@yahoo.com>
2022-02-18 20:39:17 -0500

    updated schema file

--- db/connections.js
+++ db/connections.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
       host: "localhost",
       user: "root",
       password: "Ta1!SQLTa1!",
-      database: "election",
+      database: "company",
     },
     console.log("Connected to the Company database.")
   );
--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
 
 
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE roles (
     role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     role_title VARCHAR(30) NOT NULL,
-    salary DECIMA(10,2)) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department_id(id) ON DELETE SET NULL
-)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
+);
 
 CREATE TABLE employees (
     employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER
-    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-)
+    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
+);
